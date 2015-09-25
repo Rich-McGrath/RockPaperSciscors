8629efec17a8f3e8143ecb50c7b9fd998cfa03a2
Rich-McGrath <richmcgrath@gmail.com>
2015-09-25 11:27:34 -0400

    Fixed a spelling mistake and add some blan lines

--- rps.rb
+++ rps.rb
@@ -1,13 +1,16 @@
 # Rock Paper Siscors Game
 
 CHOICES = { 'r' => 'Rock', 'p' => 'Paper', 's' => 'Sisscors' }
-puts 'Welcome to Rock Paper Sisscors'
+print 'Welcome to Rock Paper Sisscors!'
+puts ''
+puts ''
 
 loop do
   # Play picks their fate
   begin
   puts 'Decide your fate! Please choose Rock, Paper or Sisscors'
-  puts 'Please select one: R = Rocl, P = Paper, S = Sisscors'
+  puts ''
+  puts 'Please select one: R = Rock, P = Paper, S = Sisscors'
   user_choice = gets.chomp.to_s.uppercase
   end until CHOICES.keys.include?(user_choice)
 
