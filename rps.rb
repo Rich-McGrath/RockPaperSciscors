4a31ec290ffbc3047217f53c3ee83e0cff2b22a1
Rich-McGrath <richmcgrath@gmail.com>
2015-09-25 11:22:22 -0400

    Created RPS game logic and tell the user if they won or not

--- rps.rb
+++ rps.rb
@@ -10,3 +10,18 @@ loop do
   puts 'Please select one: R = Rocl, P = Paper, S = Sisscors'
   user_choice = gets.chomp.to_s.uppercase
   end until CHOICES.keys.include?(user_choice)
+
+  # computer makes a picks
+  computer_choice = CHOICES.keys.sample
+
+  if user_choice == computer_choice
+    puts "It's a time! You both selected #{user_choice}"
+  elsif (player_choice == 'p' && computer_choice == 'r') ||
+        (player_choice == 'r' && computer_choice == 's') ||
+        (player_choice == 's' && computer_choice == 'p')
+    puts 'You won!'
+  else
+    puts 'The Computer won!'
+    puts "The Computer selected #{computer_choice}"
+  end
+end
