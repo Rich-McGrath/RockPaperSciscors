518907a9ec5257644bf4cdf948cd2cc5d453d9cd
Rich-McGrath <richmcgrath@gmail.com>
2015-09-25 11:38:39 -0400

    Fixed method name

--- rps.rb
+++ rps.rb
@@ -8,20 +8,20 @@ puts ''
 loop do
   # Play picks their fate
   begin
-  puts 'Decide your fate! Please choose Rock, Paper or Sisscors'
-  puts ''
-  puts 'Please select one: R = Rock, P = Paper, S = Sisscors'
-  user_choice = gets.chomp.to_s.uppercase
+    puts 'Decide your fate! Please choose Rock, Paper or Sisscors'
+    puts ''
+    puts 'Please select one: R = Rock, P = Paper, S = Sisscors'
+    user_choice = gets.chomp.downcase
   end until CHOICES.keys.include?(user_choice)
 
   # computer makes a picks
   computer_choice = CHOICES.keys.sample
 
   if user_choice == computer_choice
-    puts "It's a time! You both selected #{user_choice}"
-  elsif (player_choice == 'p' && computer_choice == 'r') ||
-        (player_choice == 'r' && computer_choice == 's') ||
-        (player_choice == 's' && computer_choice == 'p')
+    puts "It's a tim! You both selected #{CHOICES.values(user_choice)}"
+  elsif (user_choice == 'p' && computer_choice == 'r') ||
+        (user_choice == 'r' && computer_choice == 's') ||
+        (user_choice == 's' && computer_choice == 'p')
     puts 'You won!'
   else
     puts 'The Computer won!'
